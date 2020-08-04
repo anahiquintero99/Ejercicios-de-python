088fbe8020098295501f5b3df2636c2125eeed5e
Anahi <anitaquintero99@gmail.com>
2020-08-03 23:16:19 -0500

    read.py

---
+++ read.py
@@ -0,0 +1,13 @@
+
+def run():
+    counter = 0
+    with open('aleph.txt') as f:
+        for line in f:
+            counter += line.count('Beatriz')
+
+    print(f'Beatriz se encuentra {counter} en el texto')
+
+
+
+if __name__ == "__main__":
+    run()
