b09cda3be700dfa96a7bbe7e29983a7d7c4dba12
Anahi <anitaquintero99@gmail.com>
2020-08-03 23:44:56 -0500

    found letter30

---
+++ found_letter30.py
@@ -0,0 +1,9 @@
+#De una cadena de caracteres indica cuantas letras'e' tiene. 
+
+def main():
+    string = input('Introduce una cadena: ')
+    print(f'La cadena contiene {string.count('e')} letras e')
+
+
+if __name__ == "__main__":
+    main()
