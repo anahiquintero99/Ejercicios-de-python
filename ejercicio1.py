194c1871fe89d7da4dc2ed2a1c82b623af49d674
Anahi <anitaquintero99@gmail.com>
2020-08-03 23:49:09 -0500

    Programa para multiplicar valores por 10

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+def run():
+    a = int(input('Valor a: '))
+    b = int(input('Valor b: '))
+    c = int(input('Valor c: '))
+    d = int(input('Valor d: '))
+
+    print(f' a = {a*10}\n b = {b*10}\n c = {c*10}\n d = {d*10}')
+
+if __name__ == "__main__":
+    print('Ingresa los valores para multiplicarlos por 10.')
+    run()
